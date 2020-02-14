addd69441fa059dfcb598881e6fc68f0be347e30
DelzG <60347749+DelzG@users.noreply.github.com>
2020-02-14 14:39:00 +0100

    Update NIF.java

--- src/agendacoop/NIF.java
+++ src/agendacoop/NIF.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package agendacoop;
+package agenda;
 
-/**
- *
- * @author matad
- */
-public class NIF {
+
+public class nif {
+
+private int NIF;
+private char letra;
+
+private nif(){}
+
+    public nif(int NIF, char letra) {
+        this.NIF = NIF;
+        this.letra = letra;
+    }
+
+    public int getNIF() {
+        return NIF;
+    }
+
+    public void setNIF(int NIF) {
+        this.NIF = NIF;
+    }
+
+    public char getLetra() {
+        return letra;
+    }
+
+    public void setLetra(char letra) {
+        this.letra = letra;
+    }
+
+    @Override
+    public String toString() {
+        return "nif{" + "NIF=" + NIF + ", letra=" + letra + '}';
+    }
+
+
     
 }
